04c3d4a1ee21d96bceee215fb21c713d04ed2a35
Greg Johnston <gregmj.sco@gmail.com>
2024-06-09 17:17:06 +0900

    Increase Hero heading font size

--- src/components/styles/Hero.style.jsx
+++ src/components/styles/Hero.style.jsx
@@ -2,6 +2,14 @@ import styled from "styled-components";
 
 export const StyledHero = styled.section`
     flex: 1;
+
+    h1 {
+        font-size: var(--size-4xl);
+    }
+
+    p {
+        font-size: var(--size-base);
+    }
 `
 
 export const StyledImage = styled.img`
