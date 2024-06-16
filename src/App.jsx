e7ea016be8cd680f8d0ebcd9da390e0f88ecbf25
Greg Johnston <gregmj.sco@gmail.com>
2024-06-16 09:52:18 +0900

    Fix some issues implementing React Router, know loads homepage correctly

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Home from './components/Home';
 import GlobalStyles from './components/styles/Global'
 
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Navbar from './components/styles/Navbar';
 import Shop from './components/Shop';
 
@@ -11,20 +11,11 @@ function App() {
   return (
     <>
     <GlobalStyles></GlobalStyles>
-      <Router>
-        <Navbar></Navbar>
-        <Switch>
-          <Route exact path = '/'>
-            <Home></Home>
-          </Route>
-          <Route path = '/shop'>
-            <Shop></Shop>
-          </Route>
-        </Switch>
-
-        <Home></Home>
-
-      </Router>
+    <Navbar></Navbar>
+      <Routes>
+          <Route exact path = '/' element={<Home />}></Route>
+          <Route path = '/shop' element={<Shop />}></Route>
+      </Routes>
 
 
     </>
